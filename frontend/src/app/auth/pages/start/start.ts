86e3d891969607ccfb34120e4083af96bbb95a9d
Christoph <c.wagner.germany@gmail.com>
2025-12-30 14:44:56 +0100

    comments

--- frontend/src/app/auth/pages/start/start.ts
+++ frontend/src/app/auth/pages/start/start.ts
@@ -8,14 +8,10 @@ import { Component, signal } from '@angular/core';
   styleUrl: './start.scss',
 })
 export class Start {
-  // Overlay sichtbar für 1500ms
   overlayVisible = signal(true);
-
-  // Nach 500ms beginnt die "float to corner"-Animation
   floatToCorner = signal(false);
 
   constructor() {
-    // Start-Timeline
     setTimeout(() => this.floatToCorner.set(true), 500);
     setTimeout(() => this.overlayVisible.set(false), 1500);
   }
